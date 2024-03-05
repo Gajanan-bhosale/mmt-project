76f0d159f366a4c18c193f9fd790edf491ae4570
Gajanan-bhosale <gajananbhosale.official@gmail.com>
2024-03-05 17:16:03 +0530

    first commit

---
+++ src/main/java/com/example/mmtproject/MmtProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmtProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmtProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mmtproject/MmtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mmtproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
